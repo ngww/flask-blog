9628b57cbf2abefe7c748c3267d95e08ee915169
ngww <ngww95@gmail.com>
2020-07-23 15:15:43 +0000

    flask blog updates

--- application/__init__.py
+++ application/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask
 # create a new instance of Flask and store it in app 
 app = Flask(__name__)
 # import the ./application/routes.py file
-from application import routes
+from application import route
